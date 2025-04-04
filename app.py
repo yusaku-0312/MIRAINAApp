33b57271b0643d688c87c9efa8c36c3d5c24206f
yusaku0312 <yusaku.sv0312@gmail.com>
2025-04-04 18:47:30 +0900

    Finish ver1.0

--- app.py
+++ app.py
@@ -12,16 +12,20 @@ def index():
 def services():
     return render_template('services.html')
 
+# @app.route('/works')
+# def works():
+#     return render_template('works.html')
+
 # その他のルートを追加することもできます
 # 例: 問い合わせページ
-# @app.route('/contact')
-# def contact():
-#     return render_template('contact.html')  # このテンプレートはまだ作成されていません
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')  # このテンプレートはまだ作成されていません
 
-# # エラーハンドリング - 404ページ
-# @app.errorhandler(404)
-# def page_not_found(e):
-#     return render_template('404.html'), 404  # このテンプレートはまだ作成されていません
+# エラーハンドリング - 404ページ
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404  # このテンプレートはまだ作成されていません
 
 if __name__ == '__main__':
     app.run(debug=True)
